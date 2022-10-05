b62094cbf4bcd346468630f4dfad9d51ca82e1b7
Илья Харченко <kharchenko_ilia@mail.ru>
2022-10-05 21:24:46 +0700

    second furie - done

--- Furie/Furie.py
+++ Furie/Furie.py
@@ -1,7 +1,9 @@
 import math
 import matplotlib.pyplot as plt
-
+N1 =0
 def dft(coord, isForward):
+    global N1
+    N1 = 0
     origCoord = [[]]
     origCoord.append([])
     divider = len(coord[0])
@@ -15,7 +17,7 @@ def dft(coord, isForward):
         re = 0.0
         im = 0.0
         for n in range(len(coord[0])):
-            insCount+=5
+            N1+=5
             x = coord[0][n]
             y = coord[1][n]
             arg = 2*math.pi*k*n/len(coord[0])
@@ -25,10 +27,11 @@ def dft(coord, isForward):
             im += (x*imSin + y*reCos)*numSign
         origCoord[0].append(re/divider)
         origCoord[1].append(im/divider)
-    return origCoord, insCount
+    return origCoord
 
 ###############################################
 def dftA1(data, p1, p2, isForward):
+    global N1
     furieA1=[[]]
     furieA1.append([])
     divider = p1
@@ -41,6 +44,7 @@ def dftA1(data, p1, p2, isForward):
             re = 0.0
             im = 0.0
             for j1 in range(p1):
+                N1+=5
                 x = data[0][j2 + p2*j1]
                 y = data[1][j2 + p2*j1]
                 arg = 2 * math.pi *j1*k1/p1
@@ -54,8 +58,11 @@ def dftA1(data, p1, p2, isForward):
 
 
 def dftA2(data, p1, p2, isForward):
+
     furieA2 = [[]]
     furieA2.append([])
+    global N1
+    N1 = 0
     furieA1 = dftA1(data, p1, p2, isForward)
     divider = p2
     numSign = -1
@@ -67,6 +74,7 @@ def dftA2(data, p1, p2, isForward):
             re = 0.0
             im = 0.0
             for j2 in range(p2):
+                N1+=5
                 x = furieA1[0][k1 + j2*p1]
                 y = furieA1[1][k1 + j2*p1]
                 arg = 2*math.pi*((j2/(p1*p2))*(k1+p1*k2))
@@ -82,19 +90,20 @@ def dftA2(data, p1, p2, isForward):
 
 ###############################################
 
-data = [[1,0,1,0,2,0,0,1,2,4], [0,0,0,0,0,0,0,0,0,0]]
+data = [[1,2,3,4,5,6], [0,0,0,0,0,0,0]]
 
 size = len(data[0])
-for i in range(1, math.ceil(math.sqrt(len(data[0])))+1):
+for i in range(1, math.floor(math.sqrt(len(data[0])))+1):
     if(size%i==0):
         mult1 = i
         mult2 = int(size/i)
-
+print("\n\nMult: ",mult1," ",mult2,"\n\n")
 data.append([])
 
-furieList, count = dft(data, True)
-N = count
-origCoord, count = dft(furieList, False)
+furieList= dft(data, True)
+kCount = N1
+origCoord = dft(furieList, False)
+origCount = N1
 
 print(data[0])
 
@@ -102,17 +111,22 @@ print("\nRe and Im:")
 for i in range(len(furieList[0])):
     print(round(furieList[0][i], 5), "\t", round(furieList[1][i], 5), "\n" )
 
-print("\nN = ", N,"\n Answer:")
+print("\nDir and Rev = ", kCount, "  ", origCount)
+print("\n\n Answer:")
 for i in range(len(origCoord[0])):
     print(round(origCoord[0][i], 5), "\t", round(origCoord[1][i], 5), "\n" )
 
 print("\n\n\t\t\t\t\tSecond Furie\nRe and Im:")
 furieA2 = dftA2(data, mult1, mult2, True)
+kCount = N1
 for i in range(len(furieA2[0])):
     print(round(furieA2[0][i], 5), "\t", round(furieA2[1][i], 5), "\n" )
 
 origCoord.clear()
 origCoord = dftA2(furieA2, mult1, mult2, False)
+origCount = N1
 print("\n Answer:")
 for i in range(len(origCoord[0])):
     print(round(origCoord[0][i], 5), "\t", round(origCoord[1][i], 5), "\n" )
+
+print("\nDir and Rev = ", kCount, "  ", origCount)
